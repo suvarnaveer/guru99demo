f883d8a51f491105603e030cb9faf5f52259a20f
Pankaj <Pankaj@LAPTOP-F4LAP8LF>
2018-03-27 06:47:58 +0530

    test completed

---
+++ gittest/src/test/java/Guru99demo/gittest/AppTest.java
@@ -0,0 +1,22 @@
+package Guru99demo.gittest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	WebDriver driver;
+	public void Guru99Tutorials() {
+System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
+ driver = new ChromeDriver();
+driver.manage().window().maximize();
+driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+driver.get("https://www.guru99.com/");
+String getTitle=driver.getTitle();
+System.out.println(getTitle);
+}
+}
