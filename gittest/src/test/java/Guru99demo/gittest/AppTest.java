556d275094b4f16c2479dda131eddf4d916c92ff
suvarnaveer <suvarna.veer@gmail.com>
2019-01-05 14:43:07 +0530

    added pom.xml

--- gittest/src/test/java/Guru99demo/gittest/AppTest.java
+++ gittest/src/test/java/Guru99demo/gittest/AppTest.java
@@ -4,20 +4,22 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 	WebDriver driver;
+	@Test
 	public void Guru99Tutorials() {
-System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
- driver = new ChromeDriver();
-driver.manage().window().maximize();
-driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-driver.get("https://www.guru99.com/");
-String getTitle=driver.getTitle();
-System.out.println(getTitle);
-driver.close();
-}
+		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://www.guru99.com/");
+		String getTitle=driver.getTitle();
+		System.out.println(getTitle);
+		driver.close();
+	}
 }
