2d73a506cedaf77125c98b1945ca4e36d1647cb1
suvarnaveer <suvarna.veer@gmail.com>
2019-01-05 11:37:09 +0530

    added file

--- gittest/src/test/java/Guru99demo/gittest/AppTest.java
+++ gittest/src/test/java/Guru99demo/gittest/AppTest.java
@@ -14,7 +14,7 @@ public class AppTest {
 System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
  driver = new ChromeDriver();
 driver.manage().window().maximize();
-driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 driver.get("https://www.guru99.com/");
 String getTitle=driver.getTitle();
 System.out.println(getTitle);
